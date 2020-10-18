b6824391445b9629a42f1c004bdcae81a9adee8d
Jorge Cisneros <fraxito@gmail.com>
2020-10-18 22:42:52 +0200

    añadido el estilo de bootstrap en el archivo de layout

--- resources/views/layouts/ejemplolayout.blade.php
+++ resources/views/layouts/ejemplolayout.blade.php
@@ -3,6 +3,10 @@
 <html>
     <head>
         <title>Nombre de la app - @yield('titulo')</title>
+        	    <!-- Scripts -->
+				<script src="{{ asset('js/app.js') }}" defer></script>
+				<!-- Styles -->
+				<link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         @section('barralateral')
